114e66193155edc44ed1d18530f561d43d8c8149
QuocTuan <tuannguyenquoc057@gmail.com>
2025-02-27 21:04:57 +0700

    update

---
+++ BA/src/test/java/org/easyoder/EasyOderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.easyoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
